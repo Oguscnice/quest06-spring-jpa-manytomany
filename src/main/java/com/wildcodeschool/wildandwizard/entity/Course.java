72eb4e9048109f8619f367cb8f2eb57c9566988e
Oguscnice <guillaume_cometto@hotmail.fr>
2023-03-14 16:47:53 +0100

    commit

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,8 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Course {
@@ -13,9 +12,14 @@ public class Course {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
     public Course() {
     }
-
+    public List<Wizard> getWizards(){return wizards;}
+    public void setWizards(List<Wizard> wizardsList){
+        this.wizards = wizardsList;
+    }
     public Course(String name) {
         this.name = name;
     }
